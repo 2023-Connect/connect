8236b966c29ca555d7f9bfe0b51851ed2ab823f3
w2208hd <w2208hd@gmail.com>
2023-10-30 13:55:36 +0900

    subin commit

---
+++ src/SignUp/SignUp.java
@@ -0,0 +1,17 @@
+package SignUp;
+
+import javax.swing.*;
+
+public class SignUp extends JFrame {
+
+    String title = "Connect";
+
+    public SignUp() {
+        JFrame mainFrame = new JFrame(title);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(716,620);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setResizable(false);
+    }
+
+}
